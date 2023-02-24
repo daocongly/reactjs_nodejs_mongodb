34b7b590292372142f7c9faf11eef048532025c0
cong ly <daocongly0510@gmail.com>
2023-02-24 09:03:50 +0700

    UserList

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return (
-        <h2>Users Bao Ngoc</h2>
-    )
-}
+  const USERS = [
+    {
+      id: 1,
+      name: "Bao Ngoc",
+      image:
+        "https://product.hstatic.net/1000122386/product/gw-b5600sgm-1dr_3f5e9fed1e32481f82407ddbc4cab423_master.png",
+      places: 5,
+    },
+  ];
+  return <UsersList items={USERS} />;
+};
 
 export default Users;
